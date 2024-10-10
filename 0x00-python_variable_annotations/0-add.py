48eab5a539c2b60a3cc7dcd0cf4c45b2a61e380b
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-10 09:35:41 +0000

    task no0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+module for a sum two number function.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    return the sum of  the two givin numbers.
+    """
+    return a + b
+
+
+# if __name__ == "__main__":
+#     print(add(1.11, 2.22) == 1.11 + 2.22)
+#     print(add.__annotations__)
